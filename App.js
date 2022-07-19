43a42d0da855b8d99aa81410b759331fcea31c16
adamochi <honeyrumballs@gmail.com>
2022-07-19 16:08:37 +1000

    To do app with React Native, styling done

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Dimensions,
+  // TouchableHighlight,
+  // TouchableWithoutFeedback,
+  // Pressable,
+} from "react-native";
+import { theme } from "./colours";
+
+const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const personal = () => setWorking(false);
+  const work = () => setWorking(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: working ? "palegreen" : theme.grey,
+            }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={personal}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "palegreen" : theme.grey,
+            }}
+          >
+            Personal
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput placeholder="make a to do" style={styles.input} />
+      </View>
     </View>
   );
 }
@@ -13,8 +54,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.brue,
+    alignItems: "center",
+  },
+  header: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  btnText: {
+    padding: 30,
+    paddingHorizontal: 20,
+    color: "palegreen",
+    fontSize: 44,
+    fontWeight: "500",
+  },
+  input: {
+    height: 50,
+    width: SCREEN_WIDTH - 30,
+    textAlign: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
+    borderRadius: 25,
+    fontSize: 18,
   },
 });
---
+++ colours.js
@@ -0,0 +1,5 @@
+export const theme = {
+  bg: "black",
+  grey: "#3A3D40",
+  brue: "rgb(70, 0, 200)",
+};
